ca2abd7de39909747a5a5cf911310873d4f3c1af
Prakash <prakash@neemconsulting.com>
2022-03-26 18:27:34 +0530

    feat: middleware to protect api routes

---
+++ lib/auth.ts
@@ -0,0 +1,31 @@
+import { NextApiResponse, NextApiRequest } from "next";
+import jwt from "jsonwebtoken";
+import prisma from "./prisma";
+
+export const validateRoute = (handler) => {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    const { token } = req.cookies;
+
+    if (token) {
+      let user;
+      try {
+        const { id } = jwt.verify(token, "secret");
+        user = await prisma.user.findUnique({
+          where: { id },
+        });
+
+        if (!user) {
+          throw new Error("User not found");
+        }
+      } catch (err) {
+        res.status(401);
+        res.json({ error: "Not authorized" });
+        return;
+      }
+      return handler(req, res, user);
+    }
+
+    res.status(401);
+    res.json({ error: "Not authorized" });
+  };
+};
